9d445981b57f3163bede3022a1c6cf63d41b6424
Andrés Julián Álvarez Espinosa <andresjales@gmail.com>
2022-05-11 18:53:52 -0500

    Cambios menú principal celular

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 /* Barra de navegación */
 
-let menuToggle = document.querySelector('.menu_toggle');
-let menuToggleIcon = document.querySelector('.menu_toggle i');
-let menu = document.getElementById('nav_menu');
+var menu = document.getElementById("nav_menu");
 
-menuToggle.addEventListener('click', e=>{
-    menu.classList.toggle('show');
-});
+function openMenu(){
+    menu.classList.toggle("show");
+}
